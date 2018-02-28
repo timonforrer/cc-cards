a4684ac017379b2909624dafcd2207489324c71c
[thecubix] <[timon.forrer@gmail.com]>
2018-02-28 22:18:42 +0100

    Created file for admin page

---
+++ src/components/AdminPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Admin Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
