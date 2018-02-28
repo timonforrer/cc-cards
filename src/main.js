83e17d2cd1cd60d39d6ab57cee74a253325caae9
[thecubix] <[timon.forrer@gmail.com]>
2018-02-28 23:06:23 +0100

    Disabled color (interfered with the default form color codes)

--- src/main.js
+++ src/main.js
@@ -6,15 +6,15 @@ import router from './router'
 import VueFire from 'vuefire'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
-import colors from 'vuetify/es5/util/colors'
+// import colors from 'vuetify/es5/util/colors'
 
 Vue.config.productionTip = false
 
 Vue.use(Vuetify, {
   theme: {
-    primary: colors.red.darken1,
-    secondary: colors.red.lighten4,
-    accent: colors.indigo.base
+    // primary: colors.red.darken1,
+    // secondary: colors.red.lighten4,
+    // accent: colors.indigo.base
   }
 })
 Vue.use(VueFire)
