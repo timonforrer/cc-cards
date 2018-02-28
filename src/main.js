315ccc7035fe9d0d2bb67cdae51ed9c2a6a65c48
[thecubix] <[timon.forrer@gmail.com]>
2018-02-28 22:10:17 +0100

    Added color theme

--- src/main.js
+++ src/main.js
@@ -6,10 +6,17 @@ import router from './router'
 import VueFire from 'vuefire'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import colors from 'vuetify/es5/util/colors'
 
 Vue.config.productionTip = false
 
-Vue.use(Vuetify)
+Vue.use(Vuetify, {
+  theme: {
+    primary: colors.red.darken1,
+    secondary: colors.red.lighten4,
+    accent: colors.indigo.base
+  }
+})
 Vue.use(VueFire)
 
 /* eslint-disable no-new */
