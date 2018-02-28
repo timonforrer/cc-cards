c921ba04b120fefe497ed5bb71bbedf135b1e614
[thecubix] <[timon.forrer@gmail.com]>
2018-02-28 22:44:24 +0100

    Added styling and content

--- src/components/AdminPage.vue
+++ src/components/AdminPage.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    <h1>Admin Page</h1>
+    <v-app>
+      <v-container grid-list-md text-xs-left class="container">
+        <v-layout row wrap>
+          <v-flex xs12>
+            <h3 class="headline mb-0">Adminpage</h3>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-app>
   </div>
 </template>
 
@@ -13,5 +21,10 @@ export default {
 </script>
 
 <style scoped lang="scss">
-
+.container {
+  margin: 0 auto;
+  max-width: 100%;
+  position: relative;
+  width: 800px;
+}
 </style>
