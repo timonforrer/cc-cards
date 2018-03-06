86e7b7b029b3e1aca507294e0e1c42157a7f62b2
[thecubix] <[timon.forrer@gmail.com]>
2018-03-06 23:05:37 +0100

    removed vuetify from admin section (and made globally available)

--- src/components/AdminPage.vue
+++ src/components/AdminPage.vue
@@ -1,43 +1,41 @@
 <template>
   <div>
-    <v-app>
-      <v-container grid-list-md text-xs-left class="container">
-        <v-layout row wrap>
-          <v-flex xs12>
-            <h3 class="display-3 mt-3 mb-2">Adminpage</h3>
-          </v-flex>
-          <v-flex xs6>
-            <v-card>
-              <v-card-title primary-title>
-                <div>
-                  <h3 class="headline mb-0">Add new card</h3>
-                  <div>Fill in all information and submitt to add a new card to the quiz.</div>
-                </div>
-              </v-card-title>
-              <v-container>
-                <v-form>
-                  <v-select
-                    label="Category"
-                    v-model="select"
-                    :items="category"
-                    :rules="[v => !!v || 'Item is required']"
-                    required
-                  ></v-select>
-                  <v-text-field
-                    label="Task Title"
-                    required
-                  >
-                  </v-text-field>
-                </v-form>
-              </v-container>
-              <v-card-actions>
-                <v-btn flat color="orange">Add Card</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-app>
+    <v-container grid-list-md text-xs-left class="container">
+      <v-layout row wrap>
+        <v-flex xs12>
+          <h3 class="display-3 mt-3 mb-2">Adminpage</h3>
+        </v-flex>
+        <v-flex xs6>
+          <v-card>
+            <v-card-title primary-title>
+              <div>
+                <h3 class="headline mb-0">Add new card</h3>
+                <div>Fill in all information and submitt to add a new card to the quiz.</div>
+              </div>
+            </v-card-title>
+            <v-container>
+              <v-form>
+                <v-select
+                  label="Category"
+                  v-model="select"
+                  :items="category"
+                  :rules="[v => !!v || 'Item is required']"
+                  required
+                ></v-select>
+                <v-text-field
+                  label="Task Title"
+                  required
+                >
+                </v-text-field>
+              </v-form>
+            </v-container>
+            <v-card-actions>
+              <v-btn flat color="orange">Add Card</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </div>
 </template>
 
