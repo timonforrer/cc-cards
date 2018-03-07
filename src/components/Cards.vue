745b16ee15065a82422bcf989acd77ac17278e46
Mem15-A <Mem15-A@bs-aarau.ch>
2018-03-07 09:01:37 +0100

    split 'card' into 'cards' and 'card2'

---
+++ src/components/Cards.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <div class="cc-card__wrapper">
+          <card v-for="(card, index) in cards"
+                v-if="index == activeCard || index == activeCard + 1"
+                :key="index"
+                :card="card"></card>
+        </div>
+        <button @click="[like(cards[activeCard].category), updateActive()]">like</button>
+        <button @click="[nope(cards[activeCard].category), updateActive()]">dislike</button>
+    </div>
+</template>
+
+<script>
+import ccCard from './Card2.vue'
+
+export default {
+  props: [
+    'cards',
+    'cat'
+  ],
+  components: {
+    'card': ccCard
+  },
+  data () {
+    return {
+      activeCard: 0
+    }
+  },
+  methods: {
+    updateActive: function () {
+      console.log(this.activeCard)
+      this.activeCard += 1
+    },
+    like: function (cat) {
+      console.log(cat)
+      this.$emit('like', cat)
+    },
+    nope: function (cat) {
+      this.$emit('nope', cat)
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.cc-card__wrapper {
+  height: 60vh;
+  margin-top: 5em;
+  position: relative;
+}
+</style>
