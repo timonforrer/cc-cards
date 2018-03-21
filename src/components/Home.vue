627e5fea6aecfea61a5436e9bcf9d0196c7b8e71
Mem15-A <Mem15-A@bs-aarau.ch>
2018-03-21 08:49:21 +0100

    Added Home component

---
+++ src/components/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hello">
+      <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      title: 'Nachricht'
+    }
+  }
+}
+</script>
+
+<style>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
