b0bfafc6c7992e5299071cc1377ca3456a94efb0
[thecubix] <[timon.forrer@gmail.com]>
2018-03-21 22:35:22 +0100

    added the swipe logic

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,13 +1,26 @@
 <template>
   <div>
+
     <div class="cc-card-wrapper">
+
       <cc-card
         v-for="(card, index) in cards"
         v-if="index == activeCard || index == activeCard + 1"
         :key="card.id"
-        :card="card">
+        :card="card"
+        @swipedRight="[like(card.category), updateActive()]"
+        @swipedLeft="[nope(card.category), updateActive()]">
       </cc-card>
+
     </div>
+
+    <div class="aligner">
+      <div class="center">
+      <button @click="[like(cards[activeCard].category), updateActive()]">Like</button>
+      <button @click="[nope(cards[activeCard].category), updateActive()]">Nope</button>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -26,6 +39,17 @@ export default {
     return {
       activeCard: 0
     }
+  },
+  methods: {
+    like (cat) {
+      this.$emit('like', cat)
+    },
+    nope (cat) {
+      this.$emit('nope', cat)
+    },
+    updateActive () {
+      this.activeCard += 1
+    }
   }
 }
 </script>
@@ -35,5 +59,14 @@ export default {
   min-height: 80vh;
   margin-top: 5em;
   position: relative;
+}
+
+.aligner {
+  display: flex;
+  justify-content: center;
+}
+
+.center {
+  display: inline-block;
 }
 </style>
