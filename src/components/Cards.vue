74afec04ca0d99472e95089e6376a64cedd73a05
Mem15-A <Mem15-A@bs-aarau.ch>
2018-03-28 10:17:58 +0200

    Added result logic and render function

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -12,12 +12,18 @@
         @swipedLeft="[nope(card.category), updateActive()]">
       </cc-card>
 
+      <cc-result
+       v-if="resultShown"
+       :value="value"
+       :message="message"
+       class="result"></cc-result>
+
     </div>
 
     <div class="aligner">
       <div class="center">
-      <button @click="[like(cards[activeCard].category), updateActive()]">Like</button>
-      <button @click="[nope(cards[activeCard].category), updateActive()]">Nope</button>
+        <button @click="[like(cards[activeCard].category), updateActive()]">Like</button>
+        <button @click="[nope(cards[activeCard].category), updateActive()]">Nope</button>
       </div>
     </div>
 
@@ -26,6 +32,7 @@
 
 <script>
 import Card from './Card.vue'
+import Result from './Result'
 
 export default {
   name: 'Cards',
@@ -33,22 +40,42 @@ export default {
     'cards'
   ],
   components: {
-    'cc-card': Card
+    'cc-card': Card,
+    'cc-result': Result
   },
   data () {
     return {
-      activeCard: 0
+      activeCard: 0,
+      resultShown: false,
+      likes: 0,
+      value: 0,
+      message: 'yes'
     }
   },
   methods: {
     like (cat) {
       this.$emit('like', cat)
+      this.likes = this.likes + 1
     },
     nope (cat) {
       this.$emit('nope', cat)
     },
     updateActive () {
+      var maxLikes = this.cards.length
       this.activeCard += 1
+      this.value = ((this.likes / maxLikes) * 100)
+
+      console.log(this.value)
+
+      if (this.activeCard === this.cards.length) {
+        this.resultShown = true
+      }
+
+      if (this.value > 50) {
+        this.message = 'It\'s a match'
+      } else {
+        this.message = 'Mediamatiker auf den 2. Blick?'
+      }
     }
   }
 }
@@ -68,5 +95,14 @@ export default {
 
 .center {
   display: inline-block;
+}
+
+.result {
+  position: relative;
+  display: block;
+  margin: 0 auto;
+  width: 200px;
+  z-index: 3;
+  top: 8em;
 }
 </style>
