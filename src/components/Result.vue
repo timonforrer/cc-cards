beb417926252cb80a8bff56138a06d5a52f3aa32
Mem15-A <Mem15-A@bs-aarau.ch>
2018-03-28 09:01:10 +0200

    added result component

---
+++ src/components/Result.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <vue-circle
+      :progress="value"
+      :size="100"
+      :start-angle="4.75"></vue-circle>
+  </div>
+</template>
+
+<script>
+
+import VueCircle from 'vue2-circle-progress'
+
+export default {
+  name: 'Result',
+  components: {
+    VueCircle
+  },
+  data () {
+    return {
+      value: 100
+    }
+  },
+  methods: {}
+}
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
